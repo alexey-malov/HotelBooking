1d52d0d0ff2eb5a6cfc159a8dadaf19a9a65c5c1
Alexey Malov <malov.aleksey@gmail.com>
2020-05-22 23:13:07 +0300

    Реализован каркас чтения данных

---
+++ HotelBooking/HotelBooking.cpp
@@ -0,0 +1,78 @@
+// HotelBooking.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <string_view>
+
+using namespace std;
+using namespace std::literals;
+using Time = int64_t;
+using ClientId = uint32_t;
+using RoomCount = unsigned;
+
+class BookingService
+{
+public:
+};
+
+void Run(istream& input, ostream& output, BookingService& service)
+{
+	string line;
+	getline(input, line);
+	unsigned size = std::stoul(line);
+	for (unsigned i = 0; i < size; ++i)
+	{
+		getline(input, line);
+		istringstream lineStream(line);
+		string query;
+		lineStream >> query;
+
+		string hotelName;
+		if (query == "BOOK"sv)
+		{
+			Time time;
+			ClientId clientId;
+			RoomCount roomCount;
+			if (!(lineStream >> time >> hotelName >> clientId >> roomCount))
+			{
+				throw runtime_error("BOOK query syntax error");
+			}
+		}
+		else if (query == "CLIENTS"sv)
+		{
+			if (!(lineStream >> hotelName))
+			{
+				throw runtime_error("CLIENTS query syntax error");
+			}
+		}
+		else if (query == "ROOMS"sv)
+		{
+			if (!(lineStream >> hotelName))
+			{
+				throw runtime_error("ROOMS query syntax error");
+			}
+		}
+		else
+		{
+			throw runtime_error("Unknown query " + query);
+		}
+	}
+}
+
+int main(int /*argc*/, char* /*argv*/[])
+{
+	try
+	{
+		BookingService service;
+		Run(cin, cout, service);
+		return EXIT_SUCCESS;
+	}
+	catch (const exception& e)
+	{
+		cerr << e.what() << "\n";
+		return EXIT_FAILURE;
+	}
+}
