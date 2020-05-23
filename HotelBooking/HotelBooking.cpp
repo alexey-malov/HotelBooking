6c54480878055c48e08a0f473315ee7050ce1daa
Alexey Malov <malov.aleksey@gmail.com>
2020-05-23 06:06:18 +0300

    Удален закомментированный код

--- HotelBooking/HotelBooking.cpp
+++ HotelBooking/HotelBooking.cpp
@@ -2,56 +2,6 @@
 //
 #include "HotelBooking.h"
 
-
-//void Run(std::istream& input, std::ostream& output, BookingService& service)
-//{
-//	using namespace std;
-//
-//	string line;
-//	getline(input, line);
-//	unsigned size = std::stoul(line);
-//	for (unsigned i = 0; i < size; ++i)
-//	{
-//		getline(input, line);
-//		istringstream lineStream(line);
-//		string query;
-//		lineStream >> query;
-//
-//		string hotelName;
-//		if (query == "BOOK"sv)
-//		{
-//			Time time;
-//			ClientId clientId;
-//			RoomCount roomCount;
-//			if (!(lineStream >> time >> hotelName >> clientId >> roomCount))
-//			{
-//				throw runtime_error("BOOK query syntax error");
-//			}
-//			service.Book(time, hotelName, clientId, roomCount);
-//		}
-//		else if (query == "CLIENTS"sv)
-//		{
-//			if (!(lineStream >> hotelName))
-//			{
-//				throw runtime_error("CLIENTS query syntax error");
-//			}
-//			output << service.GetDistinctClientCountWithinTimeSpan(hotelName) << "\n";
-//		}
-//		else if (query == "ROOMS"sv)
-//		{
-//			if (!(lineStream >> hotelName))
-//			{
-//				throw runtime_error("ROOMS query syntax error");
-//			}
-//			output << service.GetBookedRoomCountWithinTimeSpan(hotelName) << "\n";
-//		}
-//		else
-//		{
-//			throw runtime_error("Unknown query " + query);
-//		}
-//	}
-//}
-
 int main(int /*argc*/, char* /*argv*/[])
 {
 	using namespace std;
